d7f96579cc792629fb6ea06421b3d2d61ba9ce38
usiege <jianin45@qq.com>
2017-12-29 00:06:10 +0800

    demo
    游戏Demo都放在这里

---
+++ RainCat-smashing-magazine-initial-code/RainCat/DemoScene.swift
@@ -0,0 +1,14 @@
+//
+//  DemoScene.swift
+//  RainCat
+//
+//  Created by charles on 2017/12/26.
+//  Copyright © 2017年 Thirteen23. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class DemoScene: SKScene {
+
+}
